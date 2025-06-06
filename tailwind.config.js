1d7693b0e1eef32602544d56a5a5989b4e69299c
Christian Andreas Westby <144798762+ChristianWestby@users.noreply.github.com>
2025-06-06 22:49:28 +0200

    Første commit med Tailwind 3.3.3 React 18.2 og Vite.

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+// tailwind.config.js
+export default {
+  content: ['./index.html', './src/**/*.{js,jsx}'],
+  theme: {
+    extend: {
+      colors: {
+        beige: '#f5f5dc',
+      },
+    },
+  },
+  plugins: [],
+};
